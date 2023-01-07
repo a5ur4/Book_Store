f212e1bf5874d1a883c3358e0560804b07d047e0
Pedro Bastos <113993134+a5ur4@users.noreply.github.com>
2023-01-06 23:17:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function novidades() {
+    window.scrollTo({
+        top: 730,
+        behavior: "smooth",
+    });
+}
+
+function promocao() {
+    window.scrollTo({
+        top: 1450,
+        behavior: "smooth",
+    });
+}
